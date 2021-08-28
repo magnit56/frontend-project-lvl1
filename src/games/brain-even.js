c2ad4fdee5d796ff68336212efdaeef62d52bfa5
Samat Tulegenov <magnit56@gmail.com>
2021-08-29 04:37:22 +0500

    getRandomInt вынесена в отдельный файл для переиспользования

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,8 +1,7 @@
 import run from '../cli.js';
+import getRandomInt from '../getRandomInt.js';
 
 const isEven = (number) => number % 2 === 0;
-
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const brainEven = () => {
   const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
